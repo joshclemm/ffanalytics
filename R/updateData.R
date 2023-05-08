6e6742f2c889746e95f72986e4cc4fb90775da1a
Josh Clemm <joshclemm@joshs-air.lan>
2023-05-08 10:04:29 -0700

    Helper script that runs common commands to reload data sources and prep the data tables

---
+++ R/updateData.R
@@ -0,0 +1,20 @@
+my_scrape <- scrape_data(pos = c("QB", "RB", "WR", "TE", "K", "DST", "DL", "LB", "DB"),
+                        season = 2023,
+                       week = 0)
+
+my_projections <-  ffanalytics::projections_table(my_scrape)
+
+my_projections <- my_projections %>% ffanalytics::add_ecr()
+
+my_projections <- my_projections %>% add_risk()
+
+my_projections <- my_projections %>% ffanalytics::add_player_info()
+
+write.csv(my_projections, 'projections.csv')
+
+#library(ffanalytics)
+#library(tidyr)
+my_agg <- aggregate_stats(my_scrape)
+my_agg <- my_agg %>% ffanalytics::add_player_info()
+
+write.csv(my_agg,'agg.csv')
